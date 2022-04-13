36a5ab595d0af8a5748e299a24e45bbd5cc56b9a
Mateusz Sasor-Adamczyk <s22454@pjwstk.edu.pl>
2022-04-13 12:19:38 +0200

    start pracy nad zadaniem 3

---
+++ TPO3/src/zad1/Time.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *  @author Sasor-Adamczyk Mateusz S22454
+ *
+ */
+
+package zad1;
+
+
+public class Time {
+    public static String passed(String from, String to) {
+    }
+}
---
+++ TPO3/src/zad1/Tools.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *  @author Sasor-Adamczyk Mateusz S22454
+ *
+ */
+
+package zad1;
+
+
+public class Tools {
+    public static Options createOptionsFromYaml(String fileName) throws Exception{
+    }
+}
