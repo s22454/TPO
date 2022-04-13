20cc9593beb5cbfe113efdbd426c90e12d315385
Mateusz Sasor-Adamczyk <s22454@pjwstk.edu.pl>
2022-04-13 13:13:34 +0200

    skonczylem klase tools

--- TPO3/src/zad1/Time.java
+++ TPO3/src/zad1/Time.java
@@ -7,7 +7,16 @@
 package zad1;
 
 
+import java.time.*;
+
 public class Time {
     public static String passed(String from, String to) {
+
+        LocalDate x = LocalDate.parse(from);
+        LocalDate y = LocalDate.parse(to);
+
+        Period period = Period.between(x, y);
+
+        return period;
     }
 }
