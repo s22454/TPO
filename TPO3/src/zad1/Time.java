425ac81fb02a3c7d8e88490c2b493da3f6cf8cba
Mateusz Sasor-Adamczyk <s22454@pjwstk.edu.pl>
2022-04-13 15:24:39 +0200

    zrobilem metode passed

--- TPO3/src/zad1/Time.java
+++ TPO3/src/zad1/Time.java
@@ -8,15 +8,93 @@ package zad1;
 
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoUnit.*;
 
 public class Time {
-    public static String passed(String from, String to) {
+    public static String passed(String fromText, String toText) {
 
-        LocalDate x = LocalDate.parse(from);
-        LocalDate y = LocalDate.parse(to);
+        String res;
 
-        Period period = Period.between(x, y);
+        if (fromText.matches(".*T.*") && toText.matches(".*T.*")){
+            LocalDateTime from  = LocalDateTime.parse(fromText);
+            LocalDateTime to    = LocalDateTime.parse(toText);
+            long daysBetween    = DAYS.between(from,to);
 
-        return period;
+            res =           // from section
+                            "Od " +
+                            from.getDayOfMonth() + " " +
+                            from.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " " +
+                            from.getYear() +
+                            " (" + from.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ")" +
+                            " godz. " + from.format(DateTimeFormatter.ISO_TIME).substring(0, 5) +
+
+                            // to section
+                            " do " +
+                            to.getDayOfMonth() + " " +
+                            to.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " " +
+                            to.getYear() +
+                            " (" + to.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ")" +
+                            " godz. " + to.format(DateTimeFormatter.ISO_TIME).substring(0, 5) +
+
+                            // info section
+                            "\n - mija: " + daysBetween + ((daysBetween > 1) ? " dni" : " dzień") + ", " +
+                            "tygodni " + (((int)((daysBetween / 7.0) * 100)) / 100.0) +
+                            "\n - godzin: " + HOURS.between(from,to) + ", minut: " + MINUTES.between(from,to) +
+                            "\n - kalendarzowo: " + durationToString(Period.between(from.toLocalDate(),to.toLocalDate()));
+
+        } else {
+            LocalDate from      = LocalDate.parse(fromText);
+            LocalDate to        = LocalDate.parse(toText);
+            long daysBetween    = DAYS.between(from,to);
+
+            res =           // from section
+                            "Od " +
+                            from.getDayOfMonth() + " " +
+                            from.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " " +
+                            from.getYear() +
+                            " (" + from.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ")" +
+
+                            // to section
+                            " do " +
+                            to.getDayOfMonth() + " " +
+                            to.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " " +
+                            to.getYear() +
+                            " (" + to.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()) + ")" +
+
+                            // info section
+                            "\n - mija: " + daysBetween + ((daysBetween > 1) ? " dni" : " dzień") + ", " +
+                            "tygodni " + (((int)((daysBetween / 7.0) * 100)) / 100.0) +
+                            "\n - kalendarzowo: " + durationToString(Period.between(from,to));
+        }
+
+
+        return res;
+    }
+
+    private static String durationToString(Period period){
+        StringBuilder res = new StringBuilder();
+
+        if (period.getYears() >= 1){
+           res.append(period.getYears() + ((period.getYears() == 1)? " rok" : (period.getYears() == 2)? " lata" : " lat"));
+        }
+
+        if (period.getMonths() >= 1){
+            res.append(((res.length() > 0)? " " : "") + period.getMonths() + ((period.getMonths() == 1)? " miesiąc" : (period.getMonths() > 4)? " miecięcy" : " miesiące"));
+        }
+
+        if (period.getDays() >= 1) {
+            res.append(((res.length() > 0)? " " : "") + period.getDays() + ((period.getDays() == 1) ? " dzień" : " dni"));
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        //"2020-03-27T10:00","2020-03-28T10:00"
+        System.out.println(Time.passed("2020-03-27T10:00","2020-03-28T10:00"));
     }
 }
