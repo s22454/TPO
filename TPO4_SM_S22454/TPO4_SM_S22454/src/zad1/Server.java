c4183c3c383195ec0e0bbe965f647b7a02ce77ca
Mateusz Sasor-Adamczyk <s22454@pjwstk.edu.pl>
2022-04-27 16:13:31 +0200

    jak mi sie wydaje skonczylem implementacje klasy server

--- TPO4_SM_S22454/TPO4_SM_S22454/src/zad1/Server.java
+++ TPO4_SM_S22454/TPO4_SM_S22454/src/zad1/Server.java
@@ -9,36 +9,41 @@ package zad1;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Server {
 
     private ServerSocketChannel serverSocketChannel;
     private Selector            selector;
+    private Thread              serverThread;
     
     private String  host;
     private int     port;
     private boolean serverIsRunning;
 
-    private HashMap<String, ArrayList<String>>  logs;
-    private String                              nowLogged;
+    private ArrayList<String>                   serverLogs;
+    private HashMap<String, ArrayList<String>>  usersLogs;
+    private HashMap<SocketAddress,String>       loggedUsers;
 
     public Server(String host, int port) {
-        this.host = host;
-        this.port = port;
-        this.serverIsRunning = false;
-        this.logs = new HashMap<>();
-        nowLogged = "";
+        this.host               = host;
+        this.port               = port;
+        this.serverIsRunning    = false;
+        this.usersLogs          = new HashMap<>();
+        this.serverLogs         = new ArrayList<>();
+        this.loggedUsers        = new HashMap<>();
     }
 
     public void startServer() {
-        new Thread(() -> {
+        serverThread = new Thread(() -> {
             try {
                 serverSocketChannel = ServerSocketChannel.open();
                 serverSocketChannel.configureBlocking(false);
@@ -73,11 +78,13 @@ public class Server {
                     }
                 }
 
-//                Thread.currentThread().interrupt();
+                Thread.currentThread().interrupt();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).start();
+        });
+
+        serverThread.start();
 
         try {
             Thread.sleep(1000);
@@ -88,10 +95,26 @@ public class Server {
 
     public void stopServer() {
         serverIsRunning = false;
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        serverThread.interrupt();
     }
 
     public String getServerLog() {
-        return "log";
+        if (serverLogs.size() == 0)
+            return "";
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String s : serverLogs)
+            stringBuilder.append(s + "\n");
+
+        return stringBuilder.substring(0,stringBuilder.length() - 1);
     }
 
     private void serviceRequest(SocketChannel socketChannel){
@@ -102,38 +125,46 @@ public class Server {
             socketChannel.read(byteBuffer);
             byteBuffer.flip();
             String request = String.valueOf(StandardCharsets.UTF_8.decode(byteBuffer));
+            String nowLogged = "";
+
+            if (loggedUsers.containsKey(socketChannel.getRemoteAddress()))
+                nowLogged = loggedUsers.get(socketChannel.getRemoteAddress());
 
             if (request.matches("login\\s.*") && nowLogged.length() == 0){
 
-                logs.put(request.substring(6), new ArrayList<>());
                 nowLogged = request.substring(6);
-                logs.get(nowLogged).add("=== " + nowLogged + " log start ===");
-                logs.get(nowLogged).add("logged in");
+                usersLogs.put(nowLogged, new ArrayList<>());
+                usersLogs.get(nowLogged).add("=== " + nowLogged + " log start ===");
+                usersLogs.get(nowLogged).add("logged in");
+
+                serverLogs.add(nowLogged + " logged in at " + LocalDateTime.now().toLocalTime());
+                loggedUsers.put(socketChannel.getRemoteAddress(), nowLogged);
 
                 byteBuffer.clear();
                 socketChannel.write(ByteBuffer.wrap("logged in".getBytes(StandardCharsets.UTF_8)));
 
             } else if (request.matches("bye")){
 
-                logs.get(nowLogged).add("logged out");
-                logs.get(nowLogged).add("=== " + nowLogged + " log end ===");
-                nowLogged = "";
+                serverLogs.add(nowLogged + " logged out at " + LocalDateTime.now().toLocalTime());
+                usersLogs.get(nowLogged).add("logged out");
+                usersLogs.get(nowLogged).add("=== " + nowLogged + " log end ===");
+                loggedUsers.remove(socketChannel.getRemoteAddress());
 
                 byteBuffer.clear();
                 socketChannel.write(ByteBuffer.wrap("logged out".getBytes(StandardCharsets.UTF_8)));
 
             } else if (request.matches("bye and log transfer")){
 
-                logs.get(nowLogged).add("logged out");
-                logs.get(nowLogged).add("=== " + nowLogged + " log end ===");
+                serverLogs.add(nowLogged + " logged out at " + LocalDateTime.now().toLocalTime());
+                usersLogs.get(nowLogged).add("logged out");
+                usersLogs.get(nowLogged).add("=== " + nowLogged + " log end ===");
 
                 StringBuilder stringBuilder = new StringBuilder();
-                for (String s : logs.get(nowLogged))
+                for (String s : usersLogs.get(nowLogged))
                     stringBuilder.append(s + "\n");
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
 
-
-
-                nowLogged = "";
+                loggedUsers.remove(socketChannel.getRemoteAddress());
 
                 byteBuffer.clear();
                 socketChannel.write(ByteBuffer.wrap(stringBuilder.toString().getBytes(StandardCharsets.UTF_8)));
@@ -145,9 +176,10 @@ public class Server {
                 String to   = request.substring(x + 1);
                 String res  = Time.passed(from,to);
 
-                logs.get(nowLogged).add("Request: " + request);
-                logs.get(nowLogged).add("Result:");
-                logs.get(nowLogged).add(res);
+                serverLogs.add(nowLogged + " request at " + LocalDateTime.now().toLocalTime() + ": \"" + request + "\"");
+                usersLogs.get(nowLogged).add("Request: " + request);
+                usersLogs.get(nowLogged).add("Result:");
+                usersLogs.get(nowLogged).add(res);
 
                 byteBuffer.clear();
                 socketChannel.write(ByteBuffer.wrap(res.getBytes(StandardCharsets.UTF_8)));
